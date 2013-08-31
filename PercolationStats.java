57eba5aafbfe49dce533819f1f3cb467ecb27ba8
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-08-31 14:33:27 +0200

    add empty implementation of PercolationStats

---
+++ PercolationStats.java
@@ -0,0 +1,29 @@
+public class PercolationStats {
+    public PercolationStats(int N, int T) {
+        // perform T independent computational experiments on an N-by-N grid
+    }
+
+    public double mean() {
+        // sample mean of percolation threshold
+        return 0;
+    }
+
+    public double stddev() {
+        // sample standard deviation of percolation threshold
+        return 0;
+    }
+
+    public double confidenceLo() {
+        // returns lower bound of the 95% confidence interval
+        return 0;
+    }
+
+    public double confidenceHi() {
+        // returns upper bound of the 95% confidence interval
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // test client, described below
+    }
+}
