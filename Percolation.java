18b50c3b9e1da06516b4b7618db325dac762ca4f
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-08-31 14:25:02 +0200

    check arguments are within boundary

--- Percolation.java
+++ Percolation.java
@@ -1,28 +1,36 @@
 public class Percolation {
     private boolean[] grid;
-    private int size;
+    private int N;
     
     public Percolation(int N) {
-        size = N;
+        this.N = N;
         grid = new boolean[N*N + 1];
         // create N-by-N grid, with all sites blocked
         for (int i = 0; i <= N*N + 1; i++) {
-            grid[i] = true;
+            grid[i] = false;
         }
+    }
+
+    private void checkBounds(int i, int j) {
+        if (i < 1 || i > N) throw new IndexOutOfBoundsException("row index i out of bounds");
+        if (j < 1 || j > N) throw new IndexOutOfBoundsException("column index j out of bounds");
     }
     
     public void open(int i, int j) {
+        checkBounds(i, j);
         // open site (row i, column j) if it is not already
     }
     
     public boolean isOpen(int i, int j) {
+        checkBounds(i, j);
         // is site (row i, column j) open?
-        return grid[i*size + j*size] == false;
+        return grid[i*N + j*N] == true;
     }
     
     public boolean isFull(int i, int j) {
+        checkBounds(i, j);
         // is site (row i, column j) full?
-        return grid[i*size + j*size] == true;
+        return grid[i*N + j*N] == false;
     }
     
     public boolean percolates() {
