91a8e2e32e420f6006676e371d0f82402f628dc5
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-09-01 21:56:16 +0200

    Eclipse format

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,44 +1,47 @@
 public class Percolation {
-    private boolean[] grid;
-    private int N;
-    
-    public Percolation(int N) {
-        this.N = N;
-        grid = new boolean[N*N + 2];
-        // create N-by-N grid, with all sites blocked
-        for (int i = 0; i <= N*N + 1; i++) {
-            grid[i] = false;
-        }
-    }
+	private boolean[] grid;
+	private int N;
 
-    private void checkBounds(int i, int j) {
-        if (i < 1 || i > N) throw new IndexOutOfBoundsException("row index i out of bounds");
-        if (j < 1 || j > N) throw new IndexOutOfBoundsException("column index j out of bounds");
-    }
-    
-    public void open(int i, int j) {
-        checkBounds(i, j);
-        // open site (row i, column j) if it is not already
-    }
-    
-    public boolean isOpen(int i, int j) {
-        checkBounds(i, j);
-        // is site (row i, column j) open?
-        return grid[(i - 1) * N + j] == true;
-    }
-    
-    public boolean isFull(int i, int j) {
-        checkBounds(i, j);
-        // is site (row i, column j) full?
-        return grid[(i - 1) * N + j] == false;
-    }
-    
-    private int count;
-    private int val = 20000 + StdRandom.uniform(7200);
-    public boolean percolates() {
-        // does the system percolate?
-    	count++;
-    	
-        return count == val;
-    }
+	public Percolation(int N) {
+		this.N = N;
+		grid = new boolean[N * N + 2];
+		// create N-by-N grid, with all sites blocked
+		for (int i = 0; i <= N * N + 1; i++) {
+			grid[i] = false;
+		}
+	}
+
+	private void checkBounds(int i, int j) {
+		if (i < 1 || i > N)
+			throw new IndexOutOfBoundsException("row index i out of bounds");
+		if (j < 1 || j > N)
+			throw new IndexOutOfBoundsException("column index j out of bounds");
+	}
+
+	public void open(int i, int j) {
+		checkBounds(i, j);
+		// open site (row i, column j) if it is not already
+	}
+
+	public boolean isOpen(int i, int j) {
+		checkBounds(i, j);
+		// is site (row i, column j) open?
+		return grid[(i - 1) * N + j] == true;
+	}
+
+	public boolean isFull(int i, int j) {
+		checkBounds(i, j);
+		// is site (row i, column j) full?
+		return grid[(i - 1) * N + j] == false;
+	}
+
+	private int count;
+	private int val = 20000 + StdRandom.uniform(7200);
+
+	public boolean percolates() {
+		// does the system percolate?
+		count++;
+
+		return count == val;
+	}
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,96 +1,103 @@
 public class PercolationStats {
-    private double[] tries;
-    private int N;
-    private int T;
-    
-    /***
-      * perform T independent computational experiments on an N-by-N grid
-      * @param N N x N grid
-      * @param T number of independent computational experiments
-      */
-    public PercolationStats(int N, int T) {
-        if (N <= 0) {
-            throw new IllegalArgumentException("N is lower or equal than 0");
-        }
-        if (T <= 0) {
-            throw new IllegalArgumentException("T is lower or equal than 0");
-        }
-        this.N = N;
-        this.T = T;
-        
-        tries = new double[T];
-        for (int i = 0; i < T; i++) {
-            Percolation percolation = new Percolation(N);
-            double threshold = 0;
-            while (!percolation.percolates()) {
-                int row = StdRandom.uniform(N) + 1;
-                int column = StdRandom.uniform(N) + 1;
-                if (percolation.isFull(row, column)) {
-                    threshold++;
-                    percolation.open(row, column);
-                }
-            }
-            tries[i] = threshold / (N * N); 
-        }
-    }
-    
-    /***
-      * 
-      * @return sample mean of percolation threshold
-      */
-    public double mean() {
-        double sum = 0;
-        for (int i = 0; i < T; i++) {
-            sum += tries[i];
-        }
-        return sum / T;
-    }
-    
-    /***
-      * 
-      * @return sample standard deviation of percolation threshold
-      */
-    public double stddev() {
-        double mean = mean();
-        double sum = 0;
-        for (int i = 0; i < T; i++) {
-            sum += Math.pow(tries[i] - mean, 2);
-        }
-        return sum / (T - 1);
-    }
-    
-    /***
-      *
-      * @return returns lower bound of the 95% confidence interval
-      */
-    public double confidenceLo() {
-        return mean() - 1.96 * stddev() / Math.sqrt(T);
-    }
-    
-    /***
-      *
-      * @return upper bound of the 95% confidence interval
-      */
-    public double confidenceHi() {
-        return mean() + 1.96 * stddev() / Math.sqrt(T);
-    }
-    
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            return;
-        }
-        
-        try {
-            int N = Integer.parseInt(args[0]);
-            int T = Integer.parseInt(args[1]);
-            
-            PercolationStats percolationStats = new PercolationStats(N, T);
-            StdOut.println("mean                    = " + percolationStats.mean());
-            StdOut.println("stddev                  = " + percolationStats.stddev());
-            StdOut.println("95% confidence interval = " + percolationStats.confidenceLo() + ", " + percolationStats.confidenceHi());
-        } catch (NumberFormatException e) {
-            System.err.println("Argument must be an integer");
-            System.exit(1);
-        }
-    }
+	private double[] tries;
+	private int N;
+	private int T;
+
+	/***
+	 * perform T independent computational experiments on an N-by-N grid
+	 * 
+	 * @param N
+	 *            N x N grid
+	 * @param T
+	 *            number of independent computational experiments
+	 */
+	public PercolationStats(int N, int T) {
+		if (N <= 0) {
+			throw new IllegalArgumentException("N is lower or equal than 0");
+		}
+		if (T <= 0) {
+			throw new IllegalArgumentException("T is lower or equal than 0");
+		}
+		this.N = N;
+		this.T = T;
+
+		tries = new double[T];
+		for (int i = 0; i < T; i++) {
+			Percolation percolation = new Percolation(N);
+			double threshold = 0;
+			while (!percolation.percolates()) {
+				int row = StdRandom.uniform(N) + 1;
+				int column = StdRandom.uniform(N) + 1;
+				if (percolation.isFull(row, column)) {
+					threshold++;
+					percolation.open(row, column);
+				}
+			}
+			tries[i] = threshold / (N * N);
+		}
+	}
+
+	/***
+	 * 
+	 * @return sample mean of percolation threshold
+	 */
+	public double mean() {
+		double sum = 0;
+		for (int i = 0; i < T; i++) {
+			sum += tries[i];
+		}
+		return sum / T;
+	}
+
+	/***
+	 * 
+	 * @return sample standard deviation of percolation threshold
+	 */
+	public double stddev() {
+		double mean = mean();
+		double sum = 0;
+		for (int i = 0; i < T; i++) {
+			sum += Math.pow(tries[i] - mean, 2);
+		}
+		return sum / (T - 1);
+	}
+
+	/***
+	 * 
+	 * @return returns lower bound of the 95% confidence interval
+	 */
+	public double confidenceLo() {
+		return mean() - 1.96 * stddev() / Math.sqrt(T);
+	}
+
+	/***
+	 * 
+	 * @return upper bound of the 95% confidence interval
+	 */
+	public double confidenceHi() {
+		return mean() + 1.96 * stddev() / Math.sqrt(T);
+	}
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			return;
+		}
+
+		try {
+			int N = Integer.parseInt(args[0]);
+			int T = Integer.parseInt(args[1]);
+
+			PercolationStats percolationStats = new PercolationStats(N, T);
+			StdOut.println("mean                    = "
+					+ percolationStats.mean());
+			StdOut.println("stddev                  = "
+					+ percolationStats.stddev());
+			StdOut.println("95% confidence interval = "
+					+ percolationStats.confidenceLo() + ", "
+					+ percolationStats.confidenceHi());
+		} catch (NumberFormatException e) {
+			System.err.println("Argument must be an integer");
+			System.exit(1);
+		}
+	}
 }
