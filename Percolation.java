fbcc3d2a5f82f777b40310e29bfc3a952a807783
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-09-01 21:48:43 +0200

    implementation of PercolationStats and stub results from Percolation class

--- Percolation.java
+++ Percolation.java
@@ -4,13 +4,13 @@ public class Percolation {
     
     public Percolation(int N) {
         this.N = N;
-        grid = new boolean[N*N + 1];
+        grid = new boolean[N*N + 2];
         // create N-by-N grid, with all sites blocked
         for (int i = 0; i <= N*N + 1; i++) {
             grid[i] = false;
         }
     }
-
+    
     private void checkBounds(int i, int j) {
         if (i < 1 || i > N) throw new IndexOutOfBoundsException("row index i out of bounds");
         if (j < 1 || j > N) throw new IndexOutOfBoundsException("column index j out of bounds");
@@ -24,17 +24,21 @@ public class Percolation {
     public boolean isOpen(int i, int j) {
         checkBounds(i, j);
         // is site (row i, column j) open?
-        return grid[i*N + j*N] == true;
+        return grid[(i - 1) * N + j] == true;
     }
     
     public boolean isFull(int i, int j) {
         checkBounds(i, j);
         // is site (row i, column j) full?
-        return grid[i*N + j*N] == false;
+        return grid[(i - 1) * N + j] == false;
     }
     
+    private int count;
+    private int val = 20000 + StdRandom.uniform(7200);
     public boolean percolates() {
         // does the system percolate?
-        return false;
+        count++;
+        
+        return count == val;
     }
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,43 +1,89 @@
 public class PercolationStats {
+    private double[] tries;
+    private int N;
+    private int T;
+    
+    /***
+      * perform T independent computational experiments on an N-by-N grid
+      * @param N N x N grid
+      * @param T number of independent computational experiments
+      */
     public PercolationStats(int N, int T) {
-        // perform T independent computational experiments on an N-by-N grid
         if (N <= 0) {
             throw new IllegalArgumentException("N is lower or equal than 0");
         }
         if (T <= 0) {
             throw new IllegalArgumentException("T is lower or equal than 0");
         }
+        this.N = N;
+        this.T = T;
+        
+        tries = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation percolation = new Percolation(N);
+            double threshold = 0;
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(N) + 1;
+                int column = StdRandom.uniform(N) + 1;
+                if (percolation.isFull(row, column)) {
+                    threshold++;
+                    percolation.open(row, column);
+                }
+            }
+            tries[i] = threshold / (N * N); 
+        }
     }
-
+    
+    /***
+      * 
+      * @return sample mean of percolation threshold
+      */
     public double mean() {
-        // sample mean of percolation threshold
-        return 0.5929934999999997;
+        double sum = 0;
+        for (int i = 0; i < T; i++) {
+            sum += tries[i];
+        }
+        return sum / T;
     }
-
+    
+    /***
+      * 
+      * @return sample standard deviation of percolation threshold
+      */
     public double stddev() {
-        // sample standard deviation of percolation threshold
-        return 0.00876990421552567;
+        double mean = mean();
+        double sum = 0;
+        for (int i = 0; i < T; i++) {
+            sum += Math.pow(tries[i] - mean, 2);
+        }
+        return sum / (T - 1);
     }
-
+    
+    /***
+      *
+      * @return returns lower bound of the 95% confidence interval
+      */
     public double confidenceLo() {
-        // returns lower bound of the 95% confidence interval
-        return 0.5912745987737567;
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
     }
-
+    
+    /***
+      *
+      * @return upper bound of the 95% confidence interval
+      */
     public double confidenceHi() {
-        // returns upper bound of the 95% confidence interval
-        return 0.5947124012262428;
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
     }
-
+    
     public static void main(String[] args) {
         if (args.length != 2) {
             return;
         }
-
+        
         try {
             int N = Integer.parseInt(args[0]);
             int T = Integer.parseInt(args[1]);
-
+            
             PercolationStats percolationStats = new PercolationStats(N, T);
             StdOut.println("mean                    = " + percolationStats.mean());
             StdOut.println("stddev                  = " + percolationStats.stddev());
