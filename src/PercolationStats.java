30286ae776a76d8a9ee528bfd0a3b7653f916740
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-09-02 22:02:28 +0200

    fix checkstyle errors

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,92 +1,92 @@
 public class PercolationStats {
-	private double[] tries;
-	private int T;
+    private double[] tries;
+    private int T;
 
-	/***
-	 * perform T independent computational experiments on an N-by-N grid
-	 * 
-	 * @param N
-	 *            N x N grid
-	 * @param T
-	 *            number of independent computational experiments
-	 */
-	public PercolationStats(int N, int T) {
-		if (N <= 0) {
-			throw new IllegalArgumentException("N is lower or equal than 0");
-		}
-		if (T <= 0) {
-			throw new IllegalArgumentException("T is lower or equal than 0");
-		}
-		this.T = T;
+    /***
+     * perform T independent computational experiments on an N-by-N grid
+     * 
+     * @param N
+     *            N x N grid
+     * @param T
+     *            number of independent computational experiments
+     */
+    public PercolationStats(int N, int T) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N is lower or equal than 0");
+        }
+        if (T <= 0) {
+            throw new IllegalArgumentException("T is lower or equal than 0");
+        }
+        this.T = T;
 
-		tries = new double[T];
-		for (int i = 0; i < T; i++) {
-			Percolation percolation = new Percolation(N);
-			double threshold = 0;
-			while (!percolation.percolates()) {
-				int row = StdRandom.uniform(N) + 1;
-				int column = StdRandom.uniform(N) + 1;
-				if (percolation.isFull(row, column)) {
-					threshold++;
-					percolation.open(row, column);
-				}
-			}
-			tries[i] = threshold / (N * N);
-		}
-	}
+        tries = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation percolation = new Percolation(N);
+            double threshold = 0;
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(N) + 1;
+                int column = StdRandom.uniform(N) + 1;
+                if (percolation.isFull(row, column)) {
+                    threshold++;
+                    percolation.open(row, column);
+                }
+            }
+            tries[i] = threshold / (N * N);
+        }
+    }
 
-	/***
-	 * 
-	 * @return sample mean of percolation threshold
-	 */
-	public double mean() {
-	    return StdStats.mean(tries);
-	}
+    /***
+     * 
+     * @return sample mean of percolation threshold
+     */
+    public double mean() {
+        return StdStats.mean(tries);
+    }
 
-	/***
-	 * 
-	 * @return sample standard deviation of percolation threshold
-	 */
-	public double stddev() {
-	    return StdStats.stddev(tries);
-	}
+    /***
+     * 
+     * @return sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        return StdStats.stddev(tries);
+    }
 
-	/***
-	 * 
-	 * @return returns lower bound of the 95% confidence interval
-	 */
-	public double confidenceLo() {
-		return mean() - 1.96 * stddev() / Math.sqrt(T);
-	}
+    /***
+     * 
+     * @return returns lower bound of the 95% confidence interval
+     */
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
 
-	/***
-	 * 
-	 * @return upper bound of the 95% confidence interval
-	 */
-	public double confidenceHi() {
-		return mean() + 1.96 * stddev() / Math.sqrt(T);
-	}
+    /***
+     * 
+     * @return upper bound of the 95% confidence interval
+     */
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 
-	public static void main(String[] args) {
-		if (args.length != 2) {
-			return;
-		}
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            return;
+        }
 
-		try {
-			int N = Integer.parseInt(args[0]);
-			int T = Integer.parseInt(args[1]);
+        try {
+            int N = Integer.parseInt(args[0]);
+            int T = Integer.parseInt(args[1]);
 
-			PercolationStats percolationStats = new PercolationStats(N, T);
-			StdOut.println("mean                    = "
-					+ percolationStats.mean());
-			StdOut.println("stddev                  = "
-					+ percolationStats.stddev());
-			StdOut.println("95% confidence interval = "
-					+ percolationStats.confidenceLo() + ", "
-					+ percolationStats.confidenceHi());
-		} catch (NumberFormatException e) {
-			System.err.println("Argument must be an integer");
-			System.exit(1);
-		}
-	}
+            PercolationStats percolationStats = new PercolationStats(N, T);
+            StdOut.println("mean                    = "
+                    + percolationStats.mean());
+            StdOut.println("stddev                  = "
+                    + percolationStats.stddev());
+            StdOut.println("95% confidence interval = "
+                    + percolationStats.confidenceLo() + ", "
+                    + percolationStats.confidenceHi());
+        } catch (NumberFormatException e) {
+            StdOut.println("Argument must be an integer");
+            return;
+        }
+    }
 }
