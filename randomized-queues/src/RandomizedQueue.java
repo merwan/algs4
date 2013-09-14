fa393f19c737aad0ac7ce7a13a40e76d7cf70daa
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-09-14 23:36:38 +0200

    add empty implementation of RandomizedQueue and Subset

---
+++ randomized-queues/src/RandomizedQueue.java
@@ -0,0 +1,34 @@
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    public RandomizedQueue() {
+        // construct an empty randomized queue
+    }
+
+    public boolean isEmpty() {
+        // is the queue empty?
+        return false;
+    }
+
+    public int size() {
+        // return the number of items on the queue
+        return -1;
+    }
+
+    public void enqueue(Item item) {
+        // add the item
+    }
+
+    public Item dequeue() {
+        // delete and return a random item
+        return null;
+    }
+
+    public Item sample() {
+        // return (but do not delete) a random item
+        return null;
+    }
+
+    public java.util.Iterator<Item> iterator() {
+        // return an independent iterator over items in random order
+        return null;
+    }
+}
---
+++ randomized-queues/src/Subset.java
@@ -0,0 +1,5 @@
+public class Subset {
+    public static void main(String[] args) {
+
+    }
+}
