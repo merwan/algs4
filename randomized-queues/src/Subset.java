a1eab1a41dfaac3029b92b547947655466d443ad
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-09-18 00:15:33 +0200

    implement Subset

--- randomized-queues/src/Subset.java
+++ randomized-queues/src/Subset.java
@@ -1,5 +1,19 @@
+import java.util.Iterator;
+
 public class Subset {
     public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        String s;
+        while (!StdIn.isEmpty()) {
+            s = StdIn.readString();
+            queue.enqueue(s);
+        }
 
+        Iterator<String> iterator = queue.iterator();
+        for (int i = 0; i < k; i++) {
+            String item = iterator.next();
+            StdOut.println(item);
+        }
     }
 }
