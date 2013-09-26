6a750110e8e234625e73478115c76dd564a1ec72
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-09-26 21:56:44 +0200

    implement basic compareTo

---
+++ pattern-recognition/test/PointTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void compareDifferentY() {
+        Point point = new Point(1, 5);
+        Point point2 = new Point(1, 10);
+        assertTrue(point.compareTo(point2) < 0);
+        assertTrue(point2.compareTo(point) > 0);
+    }
+
+    @Test
+    public void compareEqualY() {
+        Point point = new Point(1, 10);
+        Point point2 = new Point(5, 10);
+        assertTrue(point.compareTo(point2) < 0);
+        assertTrue(point2.compareTo(point) > 0);
+    }
+
+    @Test
+    public void compareEqualXandY() {
+        Point point = new Point(5, 10);
+        Point point2 = new Point(5, 10);
+        assertTrue(point.compareTo(point2) == 0);
+        assertTrue(point2.compareTo(point) == 0);
+    }
+}
