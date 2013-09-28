e1f619956599fa4fee43b9f27403874bc9e032b2
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-09-28 21:52:40 +0200

    first implementation of brute algorithm

--- pattern-recognition/src/Brute.java
+++ pattern-recognition/src/Brute.java
@@ -1,4 +1,38 @@
+import java.util.Arrays;
+
 public class Brute {
     public static void main(String[] args) {
+        String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        Arrays.sort(points);
+
+        for (int i = 0; i < N; i += 1) {
+            Point p = points[i];
+            for (int j = i + 1; j < N; j += 1) {
+                Point q = points[j];
+                for (int k = j + 1; k < N; k += 1) {
+                    Point r = points[k];
+                    for (int l = k + 1; l < N; l += 1) {
+                        Point s = points[l];
+                        double slopePQ = p.slopeTo(q);
+                        double slopePR = p.slopeTo(r);
+                        double slopePS = p.slopeTo(s);
+                        if (slopePQ == slopePR && slopePQ == slopePS
+                                && slopePR == slopePS) {
+                            // points are aligned
+                            StdOut.printf("%s -> %s -> %s -> %s\n", p, q, r, s);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
