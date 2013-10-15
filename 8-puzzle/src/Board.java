981f1ee0b12311a64baad16c8a15fb5f5faf0f55
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-10-15 22:39:08 +0200

    implementation of toString

--- 8-puzzle/src/Board.java
+++ 8-puzzle/src/Board.java
@@ -1,11 +1,17 @@
 public class Board {
+    private int N;
+    private int[][] tiles;
+
     public Board(int[][] blocks) {
         // construct a board from an N-by-N array of blocks
         // (where blocks[i][j] = block in row i, column j)
+        N = blocks.length;
+        tiles = blocks;
     }
 
     public int dimension() {
         // board dimension N
+        return N;
     }
 
     public int hamming() {
@@ -32,8 +38,20 @@ public class Board {
         // all neighboring boards
     }
 
+    /*
+     * string representation of the board (non-Javadoc)
+     *
+     * @see java.lang.Object#toString()
+     */
     public String toString() {
-        // string representation of the board (in the output format specified
-        // below)
+        StringBuilder s = new StringBuilder();
+        s.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
     }
 }
