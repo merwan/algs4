b86d29d95603aa9be3b9ec7956f1692af6d4fd9a
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-10-21 21:58:47 +0200

    cowboy coding of Solver, with bugs

--- 8-puzzle/src/Solver.java
+++ 8-puzzle/src/Solver.java
@@ -1,21 +1,90 @@
 public class Solver {
+    private final MinPQ<SearchNode> minPQ;
+    private final Stack<Board> boards;
+    private int moves;
+    private boolean isSolvable;
+
+    private class SearchNode implements Comparable<SearchNode> {
+        private Board board;
+        private int moves;
+        private SearchNode previous;
+
+        SearchNode(Board board, int moves, SearchNode previous) {
+            this.board = board;
+            this.moves = moves;
+            this.previous = previous;
+        }
+
+        private int priority() {
+            return moves + board.manhattan();
+        }
+
+        @Override
+        public int compareTo(SearchNode that) {
+            if (this.priority() < that.priority()) {
+                return -1;
+            }
+            if (this.priority() > that.priority()) {
+                return +1;
+            }
+            return 0;
+        }
+    }
+
+    /*
+     * find a solution to the initial board (using the A* algorithm)
+     */
     public Solver(Board initial) {
-        // find a solution to the initial board (using the A* algorithm)
+        boards = new Stack<Board>();
+        minPQ = new MinPQ<SearchNode>();
+        moves = 0;
+        SearchNode previous = null;
+        Board board = initial;
+        SearchNode node = new SearchNode(initial, moves, previous);
+        minPQ.insert(node);
+        while (moves < 35) {
+            previous = minPQ.delMin();
+            board = previous.board;
+            if (board.isGoal()) {
+                isSolvable = true;
+                this.boards.push(board);
+                while (previous.previous != null) {
+                    previous = previous.previous;
+                    this.boards.push(previous.board);
+                }
+                return;
+            }
+            Iterable<Board> neighbors = board.neighbors();
+            moves++;
+            for (Board neighbor : neighbors) {
+                if (neighbor.equals(previous.board)) {
+                    continue;
+                }
+                SearchNode newNode = new SearchNode(neighbor, moves, previous);
+                minPQ.insert(newNode);
+            }
+        }
     }
 
+    /*
+     * is the initial board solvable?
+     */
     public boolean isSolvable() {
-        // is the initial board solvable?
-        return false;
+        return isSolvable;
     }
 
+    /*
+     * min number of moves to solve initial board; -1 if no solution
+     */
     public int moves() {
-        // min number of moves to solve initial board; -1 if no solution
-        return 0;
+        return moves;
     }
 
+    /*
+     * sequence of boards in a shortest solution; null if no solution
+     */
     public Iterable<Board> solution() {
-        // sequence of boards in a shortest solution; null if no solution
-        return null;
+        return boards;
     }
 
     /*
