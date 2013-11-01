1f24516e5b6e230a6645e3147eb9a4c882bfc5be
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-11-01 14:40:29 +0100

    add more tests for contains

--- kd-trees/test/KdTreeTests.java
+++ kd-trees/test/KdTreeTests.java
@@ -25,4 +25,32 @@ public class KdTreeTests {
 
         assertTrue(tree.contains(p));
     }
+
+    @Test
+    public void testInsertNotContains() {
+        KdTree tree = new KdTree();
+        Point2D p = new Point2D(1, 5);
+        tree.insert(p);
+
+        Point2D q = new Point2D(2, 5);
+        assertFalse(tree.contains(q));
+    }
+
+    @Test
+    public void testMultipleInsertContains() {
+        KdTree tree = new KdTree();
+        Point2D p = new Point2D(1, 5);
+        Point2D q = new Point2D(2, 5);
+        Point2D r = new Point2D(1, 3);
+        Point2D s = new Point2D(7, 9);
+        tree.insert(p);
+        tree.insert(q);
+        tree.insert(r);
+        tree.insert(s);
+
+        assertTrue(tree.contains(p));
+        assertTrue(tree.contains(q));
+        assertTrue(tree.contains(r));
+        assertTrue(tree.contains(s));
+    }
 }
