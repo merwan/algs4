6164a9145383634580f6646fe519c95381b63bd7
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-11-02 22:13:30 +0100

    refactor tests

--- kd-trees/test/KdTreeTests.java
+++ kd-trees/test/KdTreeTests.java
@@ -1,44 +1,43 @@
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class KdTreeTests {
+    private final KdTree tree = new KdTree();
+
+    private Point2D insertPoint(double x, double y) {
+        Point2D p = new Point2D(x, y);
+        tree.insert(p);
+        return p;
+    }
+
     @Test
     public void testIsEmpty() {
-        KdTree tree = new KdTree();
-
         assertTrue(tree.isEmpty());
     }
 
     @Test
     public void testSize() {
-        KdTree tree = new KdTree();
-
         assertEquals(0, tree.size());
     }
 
     @Test
     public void testInsertContains() {
-        KdTree tree = new KdTree();
-        Point2D p = new Point2D(0.1, 0.5);
-        tree.insert(p);
+        Point2D p = insertPoint(0.1, 0.5);
 
         assertTrue(tree.contains(p));
     }
 
     @Test
     public void testInsertSize() {
-        KdTree tree = new KdTree();
-        Point2D p = new Point2D(0.1, 0.5);
-        tree.insert(p);
+        insertPoint(0.1, 0.5);
 
         assertEquals(1, tree.size());
     }
 
     @Test
     public void testInsertNotContains() {
-        KdTree tree = new KdTree();
-        Point2D p = new Point2D(0.1, 0.5);
-        tree.insert(p);
+        insertPoint(0.1, 0.5);
 
         Point2D q = new Point2D(0.2, 0.5);
         assertFalse(tree.contains(q));
@@ -46,11 +45,8 @@ public class KdTreeTests {
 
     @Test
     public void testTwoInsertContains() {
-        KdTree tree = new KdTree();
-        Point2D p = new Point2D(0.1, 0.5);
-        Point2D q = new Point2D(0.2, 0.8);
-        tree.insert(p);
-        tree.insert(q);
+        Point2D p = insertPoint(0.1, 0.5);
+        Point2D q = insertPoint(0.2, 0.8);
 
         assertTrue(tree.contains(p));
         assertTrue(tree.contains(q));
@@ -58,26 +54,18 @@ public class KdTreeTests {
 
     @Test
     public void testTwoInsertSize() {
-        KdTree tree = new KdTree();
-        Point2D p = new Point2D(0.1, 0.5);
-        Point2D q = new Point2D(0.2, 0.8);
-        tree.insert(p);
-        tree.insert(q);
+        insertPoint(0.1, 0.5);
+        insertPoint(0.2, 0.8);
 
         assertEquals(2, tree.size());
     }
 
     @Test
     public void testMultipleInsertContains() {
-        KdTree tree = new KdTree();
-        Point2D p = new Point2D(0.1, 0.5);
-        Point2D q = new Point2D(0.2, 0.5);
-        Point2D r = new Point2D(0.1, 0.3);
-        Point2D s = new Point2D(0.7, 0.9);
-        tree.insert(p);
-        tree.insert(q);
-        tree.insert(r);
-        tree.insert(s);
+        Point2D p = insertPoint(0.1, 0.5);
+        Point2D q = insertPoint(0.2, 0.5);
+        Point2D r = insertPoint(0.1, 0.3);
+        Point2D s = insertPoint(0.7, 0.9);
 
         assertTrue(tree.contains(p));
         assertTrue(tree.contains(q));
@@ -87,28 +75,40 @@ public class KdTreeTests {
 
     @Test
     public void testMultipleInsertSize() {
-        KdTree tree = new KdTree();
-        Point2D p = new Point2D(0.1, 0.5);
-        Point2D q = new Point2D(0.2, 0.5);
-        Point2D r = new Point2D(0.1, 0.3);
-        Point2D s = new Point2D(0.7, 0.9);
-        tree.insert(p);
-        tree.insert(q);
-        tree.insert(r);
-        tree.insert(s);
+        insertPoint(0.1, 0.5);
+        insertPoint(0.2, 0.5);
+        insertPoint(0.1, 0.3);
+        insertPoint(0.7, 0.9);
 
         assertEquals(4, tree.size());
     }
 
     @Test
     public void testRange() {
-        KdTree tree = new KdTree();
-        Point2D p = new Point2D(0.1, 0.5);
-        tree.insert(p);
+        Point2D p = insertPoint(0.1, 0.5);
 
         Iterable<Point2D> points = tree.range(new RectHV(0, 0, 1, 1));
         for (Point2D point2d : points) {
             assertEquals(p, point2d);
         }
+    }
+
+    @Test
+    public void testNearest() {
+        Point2D p = insertPoint(0.1, 0.5);
+
+        Point2D nearest = tree.nearest(new Point2D(0.3, 0.5));
+
+        assertEquals(p, nearest);
+    }
+
+    @Test
+    public void testNearestTwoPoints() {
+        insertPoint(0.1, 0.1);
+        Point2D q = insertPoint(0.3, 0.7);
+
+        Point2D nearest = tree.nearest(new Point2D(0.3, 0.5));
+
+        assertEquals(q, nearest);
     }
 }
